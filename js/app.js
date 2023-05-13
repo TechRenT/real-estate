045c1aed61a9d0bccb0465f7596d5c7af7ae33e7
Renelle Tigue <titingrenelle@gmail.com>
2023-05-13 15:59:51 +0800

    Add chat.js to the project.

--- js/app.js
+++ js/app.js
@@ -14,9 +14,48 @@ monthlyPrincipalInterest,
 monthlyPropertyTaxes,
 monthlyHomeInsurance,
 monthlyHOA,
+labels = ["Principal & Interest", "Property Taxes", "Home Insurance", "HOA"],
+backgroundColor = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)"
+],
+
+borderColor = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)"
+];
 
 
 function getNumber(str) {
   return Number(str.replace(/[^0-9\.-]+/g, ""))
 }
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+let myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
 console.log(state);
